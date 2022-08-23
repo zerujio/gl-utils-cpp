416215987cceeef667eec00f2917af6298d7dfba
Sergio Morales <semoralesd@gmail.com>
2022-08-23 12:29:33 -0400

    ShaderProgram, updated example 01

--- include/glutils/guard.hpp
+++ include/glutils/guard.hpp
@@ -5,7 +5,7 @@
 
 namespace glutils {
 
-    /// Manages the lifetime of an OpenGL object in RAII style.
+    /// Manages the lifetime of an OpenGL object in RAII style (like std::unique_ptr).
     /**
      *
      * @tparam HandleType a type that inherits from Handle.
@@ -18,7 +18,8 @@ namespace glutils {
         Guard(Args... args) : m_handle(HandleType::create(args...)) {}
 
         /// Destroy the managed object.
-        ~Guard() {
+        ~Guard()
+        {
             HandleType::destroy(m_handle);
         }
 
@@ -26,10 +27,7 @@ namespace glutils {
         /**
          * @param other Another object guard. Will be left managing no object.
          */
-        Guard(Guard && other)  noexcept : m_handle(other.m_handle)
-        {
-            other.m_handle = HandleType();
-        }
+        Guard(Guard && other)  noexcept : m_handle(other.release()) {}
 
         /// Move assignment operator.
         /**
@@ -38,30 +36,43 @@ namespace glutils {
          * @return this object.
          */
         auto operator=(Guard && other) noexcept -> Guard&
-                {
-            HandleType::destroy(m_handle);
-            m_handle = other.m_handle;
-            other.m_handle = HandleType();
+        {
+            reset(other.release());
             return *this;
         }
 
         /// Get handle to object.
-        auto getHandle() const -> HandleType
-        {
-            return m_handle;
-        }
+        auto getHandle() const -> HandleType {return m_handle;}
 
         /// Same as getHandle()
-        auto operator*() const -> HandleType
-        {
-            return m_handle;
-        }
+        auto operator*() const -> HandleType {return m_handle;}
 
         /// Access handle.
-        auto operator->() const -> const HandleType *
+        auto operator->() const -> const HandleType * {return &m_handle;}
+
+        /// Release ownership of the managed object.
+        /**
+         * The object is not destroyed.
+         * @return A handle to the object.
+         */
+        auto release() -> HandleType
         {
-            return &m_handle;
+            auto copy = m_handle;
+            m_handle = HandleType();
+            return copy;
         }
+
+        /// Change the managed object.
+        /**
+         * The current object will be destroyed.
+         * @param new_handle A (possibly zero) handle.
+         */
+        void reset(HandleType new_handle)
+        {
+            HandleType::destroy(m_handle);
+            m_handle = new_handle;
+        }
+
     private:
         HandleType m_handle;
     };
