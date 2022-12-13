f853343aa8341de7a0164f1e462d1ba7e57e19d4
Sergio Morales <semoralesd@gmail.com>
2022-12-13 18:00:48 -0300

    Added comparison operators to Handle.

--- include/glutils/handle.hpp
+++ include/glutils/handle.hpp
@@ -7,22 +7,52 @@ struct GladGLContext;
 
 namespace glutils {
 
-    /// A wrapper around an OpenGL integer name providing an object-oriented interface.
-    class Handle {
-    public:
-        Handle() = default;
+/// A wrapper around an OpenGL integer name providing an object-oriented interface.
+class Handle {
+public:
+    constexpr Handle() = default;
 
-        /// Retrieve the underlying integer name.
-        [[nodiscard]]
-        auto getName() const -> GLuint
-        {
-            return m_name;
-        }
+    /// Retrieve the underlying integer name.
+    [[nodiscard]]
+    constexpr auto getName() const -> GLuint
+    {
+        return m_name;
+    }
 
-    protected:
-        Handle(GLuint name) : m_name(name) {}
-        GLuint m_name {0};
-    };
+protected:
+    constexpr Handle(GLuint name) : m_name(name) {}
+    GLuint m_name {0};
+};
+
+constexpr bool operator==(const Handle &lhs, const Handle &rhs)
+{
+    return lhs.getName() == rhs.getName();
+}
+
+constexpr bool operator!=(const Handle &lhs, const Handle &rhs)
+{
+    return !(rhs == lhs);
+}
+
+constexpr bool operator<(const Handle &lhs, const Handle &rhs)
+{
+    return lhs.getName() < rhs.getName();
+}
+
+constexpr bool operator>(const Handle &lhs, const Handle &rhs)
+{
+    return rhs < lhs;
+}
+
+constexpr bool operator<=(const Handle &lhs, const Handle &rhs)
+{
+    return !(rhs < lhs);
+}
+
+constexpr bool operator>=(const Handle &lhs, const Handle &rhs)
+{
+    return !(lhs < rhs);
+}
 
 } // glutils
 
